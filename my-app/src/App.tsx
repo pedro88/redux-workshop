a457f728fb4ebeb3a5c0bf6717556f2058fea6b6
pedro88 <sandrep_85@hotmail.com>
2025-05-27 10:30:52 +0200

    boiler plate

---
+++ my-app/src/App.tsx
@@ -0,0 +1,6 @@
+import './index.css'
+
+
+export const App = () => (
+ <p className='text-amber-300'>my super redux App</p>
+)
---
+++ my-app/src/app/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import type { Action } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer : {}
+})
+
+export default store
