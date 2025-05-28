b111110d497580cba9e292e9e8e5e1c05325e5f6
pedro88 <sandrep_85@hotmail.com>
2025-05-28 09:47:03 +0200

    fix small stuffs

--- my-app/src/app/redux/store.ts
+++ my-app/src/app/redux/store.ts
@@ -1,18 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit"
 import {
   useDispatch as useReduxDispatch,
   useSelector as useReduxSelector,
 } from "react-redux"
-import sliceReducer from '../redux/slice'
-import { TypedUseSelectorHook } from "react-redux";
-
+import sliceReducer from "../redux/slice"
+import { TypedUseSelectorHook } from "react-redux"
 
 export const store = configureStore({
   reducer: {
-    sliceReducer
-  }
+    slice: sliceReducer,
+  },
 })
-
 
 export type RootState = ReturnType<typeof store.getState>
 
@@ -21,7 +19,5 @@ export type AppDispatch = typeof store.dispatch
 export const useDispatch = () => useReduxDispatch<AppDispatch>()
 
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector
-
-
 
 export default store
